3dd31331f4490e2d2c3575cb706e6ceda4c3bf66
03tpm <tuxtto@gmail.com>
2024-05-28 02:40:04 +0300

    Rework

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pixel Battle
+
+Pixel Battle, developed by [eeaq](https://discordapp.com/users/192188318555832321/) for a fun game between servers, or just for drawing on pixels, everyone chooses what they like best. 
+
+### You can support the project here - 4441 1111 5365 1520
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+import React from 'react';
+import PixelGrid from './components/PixelGrid';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Pixel Battle</h1>
+      <PixelGrid />
+    </div>
+  );
+};
+
+export default App;
